4d7b4dc0c47318e97e3d26f7d3b67878fca67131
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-05-28 22:49:52 +0200

    task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/statistics';
+import { FriendList } from './FriendList/FriendList';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friendsList from '../data/friends.json';
 export const App = () => {
   return (
     <>
@@ -13,6 +15,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friendsList} />
     </>
   );
 };
